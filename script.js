97622372e5cc5eb09a1c3eb1fee8879c0dcffdf6
aselvalieva <aselvalieva@icloud.com>
2024-12-02 10:42:33 +0300

    check my homework

---
+++ script.js
@@ -0,0 +1,50 @@
+let r = 10;
+alert(r)
+
+let h = 20;
+alert(h);
+
+const age = 2007;
+alert(age);
+
+const creatorName = 'Brendan Eich';
+alert(creatorName);
+
+let x = 10;
+let y = 2;
+alert(x + y);
+alert(x - y);
+alert(x * y);
+alert(x / y);
+
+
+let result = 2 ** 5;
+alert(result);
+
+let a = 9;
+let b = 2;
+alert(a % b);
+
+
+let num = 1;
+num = +5;
+num = -3;
+num = * 7;
+num = / 3;
+num = + 1;
+num = - 1;
+alert(num)
+
+
+let age = prompt(Сколько вам лет ?);
+alert(age);
+
+const user = {
+    name: 'Ivan',
+    age: 40,
+    isAdmin: true
+};
+
+let name = prompt('Введите ваше имя:');
+alert(`Привет, $ {name}!`);
+
